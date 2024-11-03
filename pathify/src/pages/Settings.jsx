c0ef303fb3ec828c18c59daaf75a2be6445c3e71
jimin kim <jmk.jiminkim@gmail.com>
2024-11-03 15:55:51 -0800

    basic settings sidebar + navigation

--- pathify/src/pages/Settings.jsx
+++ pathify/src/pages/Settings.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "../components/Settings.css"
 
 const Settings = () => {
+    const [selectedSetting, setSelectedSetting] = useState("privacy");
 
-    return(
-        <div>
-            SS page
+    const renderSetting = () => {
+        switch (selectedSetting) {
+            case "privacy":
+                return <div>Privacy Settings</div>
+            case "light/dark mode":
+                return <div>Light and Dark Mode</div>
+            default:
+                return <div>Select a setting!</div>
+        }
+    };
+
+    return (
+        <div className="settings-page">
+            <aside className="sidebar">
+                <ul>
+                    <li onClick={() => setSelectedSetting("privacy")}>Privacy</li>
+                    <li onClick={() => setSelectedSetting("light/dark mode")}>Light and Dark Mode</li>
+                </ul>
+            </aside>
+            <main className="settings-content">
+                {renderSetting()}
+            </main>
         </div>
+
     );
-}
+};
 
 export default Settings;
