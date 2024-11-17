91896f00043304f696ad60e26af0b44cbfacf035
98coco <licoco004@gmail.com>
2024-11-17 12:53:09 -0800

    Table Created
    Created a table for listing internships and having placeholders for the current jobs on there.

--- pathify/src/pages/Home.jsx
+++ pathify/src/pages/Home.jsx
@@ -1,18 +1,67 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import './Home.css'
 
 function Home() {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     return(
         <div>
-            <h1 class ="title"> Start Your Career Journey: </h1>
-            <div className = "buttonDisplay">
-                <button className ="Button" onClick={() => navigate('/profile')}>Back to Home Page</button>
-                <button className ="Button" onClick={() => navigate('/auth')}> Sign out </button>
+            <div className='title'>
+                "Insert Name" Career Tracker
             </div>
+            <div className='table-container'>
+                <table>
+                    <thead>
+                        {/* Main Header Row */}
+                        <tr> 
+                            <th> Company </th>
+                            <th> Position Title </th>
+                            <th> Application Deadline </th>
+                            <th> Applied </th>
+                            <th> Interview </th>
+                            <th> Decision </th>
+                        </tr>
+                    </thead>
+                    {/* tbody contains our data with jobs people applied to etc.  */}
+                    <tbody> 
+                        <tr>
+                            {/* Filler until we get out SQL set up */}
+                            <td> Microsoft </td>
+                            <td> Product Manager Internship </td>
+                            <td>  November 2024 </td>
+                            <td> Applied  </td>
+                            <td> Interviewed </td>
+                            <td> Not Released </td>
+                        </tr>
+                        <tr>
+                            {/* Filler until we get out SQL set up */}
+                            <td> Microsoft </td>
+                            <td> Product Manager Internship </td>
+                            <td>  November 2024 </td>
+                            <td> Applied  </td>
+                            <td> Interviewed </td>
+                            <td> Not Released </td>
+                        </tr>
+                        <tr>
+                            {/* Filler until we get out SQL set up */}
+                            <td> Microsoft </td>
+                            <td> Product Manager Internship </td>
+                            <td>  November 2024 </td>
+                            <td> Applied  </td>
+                            <td> Interviewed </td>
+                            <td> Not Released </td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div>
+            
+            {/* <div className = "buttonDisplay">
+                <button className ="Button" onClick={() => navigate('/profile')}> Back to Home Page</button>
+                <button className ="Button" onClick={() => navigate('/auth')}> Sign out </button>
+            </div> */}
         </div>
 
         
