fc75720ed56e0cb58296ba874870e7cfa3ae983a
98coco <licoco004@gmail.com>
2024-11-17 13:29:19 -0800

    Package Json files and app.jsx

--- pathify/src/App.jsx
+++ pathify/src/App.jsx
@@ -9,7 +9,6 @@ import Navbar from './components/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import Profile from './pages/Profile.jsx'
 import Settings from './pages/Settings.jsx'
-import Authentication from './pages/Authentication.jsx'
 import Connection from './pages/Connection.jsx'
 import Login from './pages/LogIn.jsx'
 
