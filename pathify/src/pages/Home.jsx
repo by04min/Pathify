4c07bed90c8ef5bd80a1d275448248a4f6c7a1e7
jimin kim <jmk.jiminkim@gmail.com>
2024-11-23 11:52:46 -0800

    Revert "Merge branch 'main' into jimin/settings-and-navigation-bar"
    This reverts commit 782a9e092c20b7299cd345b55c930c907f252864, reversing
    changes made to f3c84a8f40280474a980f5f8b98152f5fc3fbee9.

--- pathify/src/pages/Home.jsx
+++ pathify/src/pages/Home.jsx
@@ -1,25 +1,13 @@
+
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Home.css';
-import {StatusDropdown, InterviewDropdown, DecisionDropdown} from  "../components/Dropdown";
-import DateSelector from '../components/DateSelector';
+import { AuthContext } from '../components/AuthContext.jsx';
+import './Home.css'
+
 
 function Home() {
     const navigateLogin = useNavigate();
     const {user} = useContext(AuthContext);
-  
-    const [statuses, setStatuses] = useState({
-        row1: 'Applied',
-        row2: 'Not Applied',
-        row3: 'Interviewed',
-    });
-
-    const handleStatusChange = (row, status) => {
-        setStatuses(prevStatuses => ({
-            ...prevStatuses,
-            [row]: status,
-        }));
-    };
 
     return(
         <div>
@@ -36,44 +24,53 @@ function Home() {
             <div className='title'>
                 Career Tracker
             </div>
-              <div className='table-container'>
-                  <table>
-                      <thead>
-                          {/* Main Header Row */}
-                          <tr> 
-                              <th> Company </th>
-                              <th> Position Title </th>
-                              <th> Application Deadline </th>
-                              <th> Status </th>
-                              <th> Interview </th>
-                              <th> Decision </th>
-                          </tr>
-                      </thead>
-                      {/* tbody contains our data with jobs people applied to etc.  */}
-                      <tbody> 
-                          <tr>
-                              {/* Filler until we get out SQL set up */}
-                              <td> Microsoft </td>
-                              <td> Product Manager Internship </td>
-                              <td> <DateSelector/> </td>
-                              <td> <StatusDropdown/>  </td>
-                              <td> <InterviewDropdown/> </td>
-                              <td> <DecisionDropdown/> </td>
-                          </tr>
-                          <tr>
-                              {/* Filler until we get out SQL set up */}
-                              <td> Microsoft </td>
-                              <td> Product Manager Internship </td>
-                              <td>  November 2024 </td>
-                              <td> Applied  </td>
-                              <td> Interviewed </td>
-                              <td> Not Released </td>
-                          </tr>
-                      </tbody>
-                  </table>
-              </div>
-            </div>
-          )}
+                <div className='table-container'>
+                    <table>
+                        <thead>
+                            {/* Main Header Row */}
+                            <tr> 
+                                <th> Company </th>
+                                <th> Position Title </th>
+                                <th> Application Deadline </th>
+                                <th> Applied </th>
+                                <th> Interview </th>
+                                <th> Decision </th>
+                            </tr>
+                        </thead>
+                        {/* tbody contains our data with jobs people applied to etc.  */}
+                        <tbody> 
+                            <tr>
+                                {/* Filler until we get out SQL set up */}
+                                <td> Microsoft </td>
+                                <td> Product Manager Internship </td>
+                                <td>  November 2024 </td>
+                                <td> Applied  </td>
+                                <td> Interviewed </td>
+                                <td> Not Released </td>
+                            </tr>
+                            <tr>
+                                {/* Filler until we get out SQL set up */}
+                                <td> Microsoft </td>
+                                <td> Product Manager Internship </td>
+                                <td>  November 2024 </td>
+                                <td> Applied  </td>
+                                <td> Interviewed </td>
+                                <td> Not Released </td>
+                            </tr>
+                            <tr>
+                                {/* Filler until we get out SQL set up */}
+                                <td> Microsoft </td>
+                                <td> Product Manager Internship </td>
+                                <td>  November 2024 </td>
+                                <td> Applied  </td>
+                                <td> Interviewed </td>
+                                <td> Not Released </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
