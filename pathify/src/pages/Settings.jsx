f716b056389baa427f7ee8d68b978da0bebd30a8
jimin kim <jmk.jiminkim@gmail.com>
2024-11-23 20:55:49 -0800

    Frontend Design for Privacy Page
    Created UI for privacy page. Users can interact with checkboxes to decide whether to share their email and/or their current internship applications list with other users who visit their profile on the Connections page.
    
    The 'internship application list' option features a preview for users to see what sharing the list entails.

--- pathify/src/pages/Settings.jsx
+++ pathify/src/pages/Settings.jsx
@@ -6,9 +6,17 @@ const Settings = ({ toggleTheme, navColor, updateNavColor, resetNavColor }) => {
     const [selectedSetting, setSelectedSetting] = useState("display"); // tracks what page the user has clicked on; defaults to display page
     const [toggled, setToggled] = useState(false); // tracks whether user has chosen light mode or dark mode
 
+    const [shareEmail, setShareEmail] = useState(false); // tracks whether user chooses to share their contact (email) with others
+    const [shareInternships, setShareInternships] = useState(false); // tracks whether user chooses to share their list of internships with others
+    const [showListPreview, setShowListPreview] = useState(false); // tracks whether to show or hide preview of user application list as it would appear on the connections page
+
     const clickedToggle = () => {
         setToggled(!toggled);
         toggleTheme();
+    };
+
+    const handleCheckboxChange = (setter) => (event) => {
+        setter(event.target.checked);
     };
 
     const renderSetting = () => {
@@ -42,8 +50,63 @@ const Settings = ({ toggleTheme, navColor, updateNavColor, resetNavColor }) => {
                         </button>
                     </div>
                 );
+
             case "privacy":
-                return <div>Privacy Settings</div>
+                return (
+                    <div>
+                        <h4>Privacy Settings</h4>
+                        <h2>Manage what you share on Pathify</h2>
+                        <hr className="line" />
+                        <div className="privacy-option">
+                            <div className="priv-checkbox-col">
+                                <input
+                                    type="checkbox"
+                                    checked={shareEmail}
+                                    onChange={handleCheckboxChange(setShareEmail)}
+                                />
+                            </div>
+                            <div className="priv-description-col">
+                            <div className="label-container">
+                                <label>Share my e-mail with others</label>
+                            </div>
+                                <p>Your UCLA email will be visible to other users who visit your profile through the Connections page.</p>
+                            </div>
+                        </div>
+
+
+                        <div className="privacy-option">
+                            <div className="priv-checkbox-col">
+                                <input
+                                    type="checkbox"
+                                    checked={shareInternships}
+                                    onChange={handleCheckboxChange(setShareInternships)}
+                                />
+                            </div>
+                            <div className="priv-description-col">
+                                <div className="label-container">
+                                    <label>Share my internship application list</label>
+                                </div>
+                                <p>Other users will be able to see a list of internships you are currently applying to.<br />Your application status will not be visible at any time.</p>
+                                <button
+                                    className="preview-link"
+                                    onClick={() => setShowListPreview(!showListPreview)}
+                                >
+                                    {showListPreview ? "Hide Preview" : "Show Preview"}
+                                </button>
+                                {showListPreview && (
+                                    <div className="preview-box">
+                                        <h5>Current Internship Applications:</h5>
+                                        <ul>
+                                            <li>ABC Internship Position, at XYZ Inc</li>
+                                            <li>ABC Summer Internship, at XYZ Ltd</li>
+                                            <li>ABC Position Application, at XYZ</li>
+                                        </ul>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                );
         }
     };
 
