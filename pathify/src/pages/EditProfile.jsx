7657810a2911f4058fdc5400d7c8582e587370ec
jimin kim <jmk.jiminkim@gmail.com>
2024-12-04 10:41:28 -0800

    Added date selector to edit experience section, and basic styling for page

--- pathify/src/pages/EditProfile.jsx
+++ pathify/src/pages/EditProfile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../components/AuthContext.jsx'
 import { getProfile, editProfile } from '../services/profileServices.js';
+import './EditProfile.css'
 
 const EditProfile = () => {
   const navigate = useNavigate();
@@ -41,32 +42,31 @@ const EditProfile = () => {
 
   const DeleteButton = ({index}) => {
     const handleClick = () => {         
-      event.preventDefault();
 
       if (confirm("Are you sure you want to delete this internship?")) {
         setInvisible(prevSet => new Set(prevSet.add(index)));
       }
     }
-    return(<button onClick={()=> handleClick()}>X</button>);
+    return(<button className="delete-experience" onClick={()=> handleClick()}>X</button>);
   }
 
   return (
     data == null ? <p>Loading...</p> :(
     <div>
       {/* Form for submitting jobs */}
-      <h3 className="home-form-title"> Edit Profile Page </h3>
-      <div className='home-form-container'>
-        <form className='home-form' onSubmit={handleSubmit}>
-          <label className='home-form-label' htmlFor='company-name'> Username </label>
-          <input className='home-form-input' type="text"  name="company-name" placeholder="Company Name" 
+      <h3 className="editProf-form-title"> Edit Profile Page </h3>
+      <div className='editProf-form-container'>
+        <form className='editProf-form' onSubmit={handleSubmit}>
+          <label className='editProf-form-label' htmlFor='company-name'> Username </label>
+          <input className='editProf-form-input' type="text"  name="company-name" placeholder="Company Name" 
             onChange={(e) => { setUsername(e.target.value); }} value={username}/>
 
-          <label className='home-form-label' htmlFor='position-title'> Major </label>
-          <input className='home-form-input' type="text" name="position-title" placeholder="Position Title" 
+          <label className='editProf-form-label' htmlFor='position-title'> Major </label>
+          <input className='editProf-form-input' type="text" name="position-title" placeholder="Position Title" 
             onChange={(e) => { setMajor(e.target.value); }} value={major}/>
 
-          <label className='home-form-label' htmlFor='application-deadline'> Industry</label>
-          <input className='home-form-input' type="text" name="application-deadline" placeholder="Application Deadline: MM/DD/YYYY"
+          <label className='editProf-form-label' htmlFor='application-deadline'> Industry</label>
+          <input className='editProf-form-input' type="text" name="application-deadline" placeholder="Application Deadline: MM/DD/YYYY"
             onChange={(e) => { setIndustry(e.target.value); }} value={industry}/>
           
           {!experiences ? (<div>N/A</div>) : experiences.map((row, index) => {
@@ -74,8 +74,8 @@ const EditProfile = () => {
             return (
               <div key={index}>
                 <DeleteButton index={index} />
-                  <label className='home-form-label' htmlFor='company-name'> Company </label>
-                  <input className='home-form-input' type="text"  name="company-name" placeholder="Company Name" 
+                  <label className='editProf-form-label' htmlFor='company-name'>Company</label>
+                  <input className='editProf-form-input' type="text"  name="company-name" placeholder="Company Name" 
                       onChange={(e) => { 
                         const updatedExp = [...experiences];
                         updatedExp[index] = { ...row, company: e.target.value};
@@ -83,8 +83,8 @@ const EditProfile = () => {
                       }}
                       value={row.company}/>
 
-                  <label className='home-form-label' htmlFor='company-name'> Job Title</label>
-                  <input className='home-form-input' type="text"  name="company-name" placeholder="Company Name"
+                  <label className='editProf-form-label' htmlFor='job-title'>Job Title</label>
+                  <input className='editProf-form-input' type="text"  name="job-title" placeholder="Job Title"
                       onChange={(e) => { 
                         const updatedExp = [...experiences];
                         updatedExp[index] = { ...row, job: e.target.value};
@@ -92,8 +92,8 @@ const EditProfile = () => {
                       }}
                       value={row.job}/>
 
-                  <label className='home-form-label' htmlFor='company-name'> Start date</label>
-                  <input className='home-form-input' type="text"  name="company-name" placeholder="Company Name" 
+                  <label className='editProf-form-label' htmlFor='start-date'>Start Date</label>
+                  <input className='editProf-form-input' type="date"  name="start-date" placeholder="Start Date" 
                       onChange={(e) => { 
                         const updatedExp = [...experiences];
                         updatedExp[index] = { ...row, start: e.target.value};
@@ -101,8 +101,8 @@ const EditProfile = () => {
                       }}
                       value={row.start}/>
 
-                  <label className='home-form-label' htmlFor='company-name'> End date</label>
-                  <input className='home-form-input' type="text"  name="company-name" placeholder="Company Name" 
+                  <label className='editProf-form-label' htmlFor='end-date'>End Date</label>
+                  <input className='editProf-form-input' type="date"  name="end-date" placeholder="End Date" 
                       onChange={(e) => { 
                         const updatedExp = [...experiences];
                         updatedExp[index] = { ...row, end: e.target.value};
@@ -110,8 +110,8 @@ const EditProfile = () => {
                       }}
                       value={row.end}/>
 
-                  <label className='home-form-label' htmlFor='company-name'> Description </label>
-                  <input className='home-form-input' type="text"  name="company-name" placeholder="Company Name" 
+                  <label className='editProf-form-label' htmlFor='role-description'>Description</label>
+                  <input className='editProf-form-input' type="text"  name="role-description" placeholder="Role Description" 
                       onChange={(e) => { 
                         const updatedExp = [...experiences];
                         updatedExp[index] = { ...row, description: e.target.value};
@@ -119,8 +119,8 @@ const EditProfile = () => {
                       }}
                       value={row.description}/>
 
-                  <label className='home-form-label' htmlFor='company-name'> Reflection</label>
-                  <input className='home-form-input' type="text"  name="company-name" placeholder="Company Name" 
+                  <label className='editProf-form-label' htmlFor='reflection'> Reflection</label>
+                  <input className='editProf-form-input' type="text"  name="reflection" placeholder="Reflection" 
                       onChange={(e) => { 
                           const updatedExp = [...experiences];
                           updatedExp[index] = { ...row, reflection: e.target.value};
@@ -130,7 +130,7 @@ const EditProfile = () => {
               </div>
             )} 
           )}
-          <button className="home-form-buttons" type="submit"> Done </button>
+          <button className="editProf-form-buttons" type="submit"> Done </button>
         </form>
       </div>
     </div>)
